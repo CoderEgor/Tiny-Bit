dbb519f2b33c6ae4aa6ff12dde68e1e1e38d0f57
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2022-12-11 01:36:27 +0300

    добавил движение по линии

--- main.ts
+++ main.ts
@@ -9,3 +9,14 @@ basic.forever(function () {
         Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
     }
 })
+basic.forever(function () {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 128)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 128)
+    } else {
+        Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
+    }
+})
