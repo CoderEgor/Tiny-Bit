0d7f62d643f026c76da10907e0ba21112b8f0387
CoderEgor <120256717+CoderEgor@users.noreply.github.com>
2022-12-10 15:57:22 +0300

    фонари зажигаются при приближении к препятствию

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+basic.forever(function () {
+    if (Tinybit.Ultrasonic_Car() < 15 && Tinybit.Ultrasonic_Car() > 10) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.White)
+    } else if (Tinybit.Ultrasonic_Car() <= 10 && Tinybit.Ultrasonic_Car() > 5) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
+    } else if (Tinybit.Ultrasonic_Car() <= 5) {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    } else {
+        Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+    }
+})
