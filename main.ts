94e168b9ffb72abd4ba3931a6edb23bc37954c51
andreipavlevich <73068640+andreipavlevich@users.noreply.github.com>
2022-12-11 14:17:24 +0300

    изменил цвет на RGB, добавил алгоритм остановки по препятствию

--- main.ts
+++ main.ts
@@ -1,22 +1,36 @@
+let isObstacle = 0
+let isOffRoad = 0
 basic.forever(function () {
-    if (Tinybit.Ultrasonic_Car() < 15 && Tinybit.Ultrasonic_Car() > 10) {
-        Tinybit.RGB_Car_Big(Tinybit.enColor.White)
-    } else if (Tinybit.Ultrasonic_Car() <= 10 && Tinybit.Ultrasonic_Car() > 5) {
-        Tinybit.RGB_Car_Big(Tinybit.enColor.Yellow)
-    } else if (Tinybit.Ultrasonic_Car() <= 5) {
-        Tinybit.RGB_Car_Big(Tinybit.enColor.Red)
+    if (Tinybit.Ultrasonic_Car() < 25 && Tinybit.Ultrasonic_Car() > 15) {
+        Tinybit.RGB_Car_Big2(255, 255, 255)
+        isObstacle = 0
+    } else if (Tinybit.Ultrasonic_Car() <= 15 && Tinybit.Ultrasonic_Car() > 7) {
+        Tinybit.RGB_Car_Big2(255, 255, 0)
+        isObstacle = 0
+    } else if (Tinybit.Ultrasonic_Car() <= 7) {
+        Tinybit.RGB_Car_Big2(255, 0, 0)
+        isObstacle = 1
     } else {
         Tinybit.RGB_Car_Big(Tinybit.enColor.OFF)
+        isObstacle = 0
     }
 })
 basic.forever(function () {
-    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black) && isObstacle == 0) {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 255)
+        isOffRoad = 0
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 128)
+        isOffRoad = 0
     } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
         Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 128)
+        isOffRoad = 0
     } else {
+        isOffRoad = 1
+    }
+})
+basic.forever(function () {
+    if (isObstacle == 1 || isOffRoad == 1) {
         Tinybit.CarCtrl(Tinybit.CarState.Car_Stop)
     }
 })
